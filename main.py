9a5de1a2142de84d10dd137d9de67982c427cf91
Tushar Kumar <tkgoyal104@gmail.com>
2025-07-04 18:36:42 +0530

    Initial commit-Main file updated

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter.font import names
+
 from logic.account import create_customers, create_account, deposit, withdraw
 from logic.utils import get_connection
 
@@ -26,5 +28,56 @@ def view_transactions(account_id):
         print(f"No Transaction Found")
     cursor.close()
     conn.close()
+
+def main_menu():
+    while True:
+        print("1. Create Customer")
+        print("2. Create Account")
+        print("3. Deposit")
+        print("4. Withdraw")
+        print("5. View Account Balance")
+        print("6. View Transactions")
+        print("7. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            name = input("Enter your name: ")
+            email = input("Enter your email: ")
+            password = input("Enter your password: ")
+            create_customers(name, email, password)
+
+        elif choice == "2":
+            customer_id = input("Enter your customer id: ")
+            account_type = input("Enter your account type: ")
+            create_account(customer_id, account_type)
+
+        elif choice == "3":
+            account_id = input("Enter your account id: ")
+            amount = float(input("Enter your amount to deposit: "))
+            deposit(account_id, amount)
+
+        elif choice == "4":
+            account_id = input("Enter your account id: ")
+            amount = float(input("Enter your amount to withdraw: "))
+            withdraw(account_id, amount)
+
+        elif choice == "5":
+            account_id = input("Enter your account id: ")
+            view_balance(account_id)
+
+        elif choice == "6":
+            account_id = input("Enter your account id: ")
+            view_transactions(account_id)
+
+        elif choice == "7":
+            print("Thanks for using this Bank Management System. Goodbye!")
+            break
+
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main_menu()
 
 
