d4c76d10c6c6859c3d49efb22d2a24487152c1e3
Tushar Kumar <tkgoyal104@gmail.com>
2025-07-08 11:30:45 +0530

    Initial commit-Files updated

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QStackedWidget
-from screens.welcome_screen import Welcome_screen
+
+from screens.login_screen import Loginscreen
+from screens.welcome_screen import Welcomescreen
+
 
 def main():
     app = QApplication(sys.argv)
     stacked_widget = QStackedWidget()
 
     def go_to_login():
-        print("Get started Clicked: next is login screen")
+        stacked_widget.setCurrentIndex(1)
 
-    welcome_screen = Welcome_screen(on_start_callback=go_to_login)
+    def handle_login(username, password):
+        print(f"Trying to login...{username} - {password}")
+
+    def go_to_signup():
+        print("Signup button clicked")
+
+    welcome_screen = Welcomescreen(on_start_callback=go_to_login)
+    login_screen = Loginscreen(on_login_callback=handle_login, on_signup_callback=go_to_signup)
     stacked_widget.addWidget(welcome_screen)
+    stacked_widget.addWidget(login_screen)
     stacked_widget.setFixedSize(500,500)
     stacked_widget.show()
 
--- screens/welcome_screen.py
+++ screens/welcome_screen.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QFont
 
-class Welcome_screen(QWidget):
+class Welcomescreen(QWidget):
     def __init__(self, on_start_callback):
         super().__init__()
         self.on_start_callback = on_start_callback
